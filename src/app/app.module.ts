add6ed0d3cbcb6a7b20e9983faf396ef8da36489
Farid <faridmcdato@gmail.com>
2017-10-10 17:19:23 -0700

    10/10/2017 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MAT_PLACEHOLDER_GLOBAL_OPTIONS,
+  MatInputModule,
+  MatMenuModule,
+  MatFormFieldModule
+} from '@angular/material';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -12,9 +23,27 @@ import { SignupComponent } from './pages/signup/signup.component';
     SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatFormFieldModule
+  ],
+  providers: [
+    {provide: MAT_PLACEHOLDER_GLOBAL_OPTIONS, useValue: {float: 'auto'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
